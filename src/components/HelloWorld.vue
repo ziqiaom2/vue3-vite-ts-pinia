eaa2a9a99380461fcd3819ec37b45f29465c4495
rexm <ziqiaom2@outlook.com>
2022-03-01 15:23:21 +0800

    2nd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,50 @@
 <script setup lang="ts">
 
-import {useMainStore} from '../store/index'
+import { useMainStore } from '../store/index'
 
 import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+//下面不能解构赋值，会丢失响应式,可以使用storeToRefs from pinia 这个api
+const mainStore = useMainStore()
+console.log('counter:', mainStore.counter)
 
-const {counter} = useMainStore()
-console.log('counter:', counter)
+//可以取出来state，不能取出来actions,把解构出来的数据做响应式代理
+const {counter, name,arr,count10} = storeToRefs(mainStore)
+
+const change = () => {
+
+  // 方式1
+  // mainStore.counter++
+
+  // 方式2 批量更新，有性能优化
+  // mainStore.$patch({
+  //   counter: mainStore.counter + 1,
+  //   name: 'xxxx'
+  // })
+
+  // 方式3
+  // mainStore.$patch(state =>{
+  //   state.counter++
+  //   state.name = 'yyyy'
+  //   state.arr.push(4)
+  // })
+
+  // 方式4 封装业务逻辑到actions
+  // mainStore.increment()
+  // mainStore.changeName()
+  // mainStore.arrChange()
+
+  // 方式5 actions内部使用$patch批量修改
+  mainStore.changeAll()
+}
+
+
+
+
 </script>
 
 <template>
@@ -22,12 +57,13 @@ console.log('counter:', counter)
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
   </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+  <p>
+    See
+    <code>README.md</code> for more information.
+  </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
@@ -37,7 +73,21 @@ console.log('counter:', counter)
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
-  <div>{{counter}}</div>
+  <div>{{ mainStore.counter }}</div>
+  <button @click="mainStore.increment">点击+1</button>
+  <hr />
+  <div>{{ counter }}</div>
+  <button @click="mainStore.increment">点击+1</button>
+  <hr />
+  <div>{{ name }}</div>
+  <div>{{ counter }}</div>
+  <button @click="change">点击+1并修改名称</button>
+  <hr />
+  <div>{{ arr }}</div>
+  <button @click="change">点击数组加入</button>
+  <hr />
+  <div>{{ count10 }}</div>
+
 </template>
 
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,15 +3,38 @@ import { defineStore } from 'pinia'
 export const useMainStore = defineStore('main',{
   state: ()=>{
     return {
-      counter:100
+      counter:100,
+      name: 'aabb',
+      arr: [1,2,3]
     }
   },
 
 getters: {
+  // count10 (state){
+  //   return state.counter + 10
+  // }
 
+  count10 ():number {
+    return this.counter + 20
+  }
 },
 actions:{
-
+  increment (){
+    this.counter++
+  },
+  changeName (){
+    this.name = 'qqqq'
+  },
+  arrChange(){
+    this.arr.push(5) 
+  },
+  changeAll (){
+    this.$patch(state =>{
+      this.counter++
+      this.name = 'qqqq'
+      this.arr.push(5)
+    })
+  }
 }
 
 })
