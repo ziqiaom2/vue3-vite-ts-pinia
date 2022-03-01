3e33e88b93c45c1e0441b3df62e1af4da5ee0be0
rexm <ziqiaom2@outlook.com>
2022-03-01 14:30:41 +0800

    1st

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+
+export const useMainStore = defineStore('main',{
+  state: ()=>{
+    return {
+      counter:100
+    }
+  },
+
+getters: {
+
+},
+actions:{
+
+}
+
+})
